2de1a173db8dab23aa3e5a5ff396af340c7338ca
Eric Zhang <ekzhang1@gmail.com>
2020-11-21 20:42:29 -0600

    Update wine glass example

--- examples/wine_glass.rs
+++ examples/wine_glass.rs
@@ -1,66 +1,70 @@
+use image::{
+    codecs::hdr::{HdrDecoder, HdrMetadata},
+    ImageResult, Rgb,
+};
+use std::io::BufReader;
 use std::time::Instant;
 
 use rpt::*;
 
+fn rgb_to_color(rgb: Rgb<f32>) -> Color {
+    glm::vec3(rgb.0[0] as f64, rgb.0[1] as f64, rgb.0[2] as f64)
+}
+
+fn load_hdr(url: &str) -> ImageResult<Hdri> {
+    let reader = ureq::get(url).call().into_reader();
+    let decoder = HdrDecoder::new(BufReader::new(reader))?;
+    let HdrMetadata { width, height, .. } = decoder.metadata();
+    let pix = decoder.read_image_hdr()?;
+    Ok(Hdri::new(
+        width,
+        height,
+        pix.into_iter().map(rgb_to_color).collect(),
+    ))
+}
+
 fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
 
+    let hdri = load_hdr("https://hdrihaven.com/files/hdris/ballroom_2k.hdr")?;
+
     let mut scene = Scene::new();
+    scene.environment = Environment::Hdri(hdri);
+
+    scene.add(
+        Object::new(load_obj("examples/wine_glass.obj")?).material(Material::clear(1.5, 0.0001)),
+    );
+    scene.add(
+        Object::new(polygon(&[
+            glm::vec3(-10.0, 0.0, -10.0),
+            glm::vec3(-10.0, 0.0, 10.0),
+            glm::vec3(10.0, 0.0, 10.0),
+            glm::vec3(10.0, 0.0, -10.0),
+        ]))
+        .material(Material::diffuse(hex_color(0x6f5d48))),
+    );
 
     scene.add(
         Object::new(
-            load_obj("examples/wine_glass.obj")?
-                .scale(&glm::vec3(0.2, 0.2, 0.2))
-                .translate(&glm::vec3(0.0, -1.12, 0.0)),
+            sphere()
+                .scale(&glm::vec3(3.0, 3.0, 3.0))
+                .translate(&glm::vec3(11.15, 13.739, -4.9325)),
         )
-        .material(Material::clear(1.5, 0.01)),
-    );
-    scene.add(
-        Object::new(plane(glm::vec3(0.0, 1.0, 0.0), -1.0))
-            .material(Material::diffuse(hex_color(0xaaaaaa))),
-    );
-    scene.add(
-        Object::new(plane(glm::vec3(0.0, 1.0, 0.0), 1.0))
-            .material(Material::diffuse(hex_color(0xaaaaaa))),
-    );
-    scene.add(
-        Object::new(plane(glm::vec3(0.0, 0.0, 1.0), -1.0))
-            .material(Material::diffuse(hex_color(0xaaaaaa))),
-    );
-    scene.add(
-        Object::new(plane(glm::vec3(0.0, 0.0, 1.0), 3.0))
-            .material(Material::diffuse(hex_color(0xaaaaaa))),
-    );
-    scene.add(
-        Object::new(plane(glm::vec3(1.0, 0.0, 0.0), -1.0))
-            .material(Material::diffuse(hex_color(0xff0000))),
-    );
-    scene.add(
-        Object::new(plane(glm::vec3(1.0, 0.0, 0.0), 1.0))
-            .material(Material::diffuse(hex_color(0x00ff00))),
+        .material(Material::light(hex_color(0xFFFFFF), 200.0)),
     );
 
-    scene.add(Light::Object(
-        Object::new(polygon(&[
-            glm::vec3(-0.25, 0.99, -0.25),
-            glm::vec3(0.25, 0.99, -0.25),
-            glm::vec3(0.25, 0.99, 0.25),
-            glm::vec3(-0.25, 0.99, 0.25),
-        ]))
-        .material(Material::light(hex_color(0xFFFEFA), 40.0)),
-    ));
-
+    let eye = glm::vec3(5.530, 4.375, 5.384);
     let camera = Camera::look_at(
-        glm::vec3(0.0, 0.0, 2.95),
-        glm::vec3(0.0, 0.0, 0.0),
+        eye,
+        eye + glm::vec3(-0.6962, -0.3754, -0.6119),
         glm::vec3(0.0, 1.0, 0.0),
-        glm::quarter_pi(),
+        0.6911,
     );
 
     let mut time = Instant::now();
     Renderer::new(&scene, camera)
-        .width(800)
-        .height(800)
+        .width(960)
+        .height(540)
         .max_bounces(6)
         .num_samples(100)
         .filter(Filter::Box(1))
